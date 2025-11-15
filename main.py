5298d76c0d628ad37337a82bc2fbdbde0968f24a
N-K1204 <m1m02530145@oit.ac.jp>
2025-11-15 23:15:26 +0900

    Fix gunicorn and requirements

--- main.py
+++ main.py
@@ -130,4 +130,5 @@ def logs():
         return f"エラー内容: {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
